00d3fbd29f6c709d9782f1a2f7b9ca4ff83b0721
Pavel Sutyrin <pavel.sutyrin@gmail.com>
2022-01-24 12:21:00 +0300

    failing request

--- web/tests/conftest.py
+++ web/tests/conftest.py
@@ -3,3 +3,8 @@ import pytest
 pytestmark = [
     pytest.mark.integration_test,
 ]
+
+
+@pytest.fixture
+def base_url():
+    return 'http://localhost:8000'
--- web/tests/simple_t.py
+++ web/tests/simple_t.py
@@ -1,2 +1,8 @@
-def returns_404__if_requested_invalid_url_t():
+import requests as requests
+
+
+def returns_404__if_requested_invalid_url_t(base_url):
+    r = requests.get(base_url + '/invalid')
+
+    assert r.status_code == 404
     assert False, 'wip'
