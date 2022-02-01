98b947866269c16347e9d925e4519e6cb64e2c15
Pavel Sutyrin <pavel.sutyrin@gmail.com>
2022-02-01 14:30:37 +0300

    simplest working test

--- web/app.py
+++ web/app.py
@@ -1,15 +1,29 @@
 from flask import Flask, abort
 
+data = None
+
 
 def create_app(pytest=False):
     app = Flask(__name__)
 
     @app.get("/correlation")
     def correlation():
+        if data:
+            return {
+                'user_id': 1,
+                'x_data_type': 'sleep',
+                'y_data_type': 'eat',
+                'correlation': {
+                    'value': 1.0,
+                    'p_value': 1.0,
+                },
+            }
         abort(404)
 
     @app.post("/calculate")
     def calculate():
+        global data
+        data = True
         abort(404)
 
     if pytest:
--- web/tests/compete_flow_t.py
+++ web/tests/compete_flow_t.py
@@ -15,9 +15,16 @@ def saves_and_retrieves_simple_data_t(app_url):
             ],
         },
     }
-
     correlation_input = {'x_data_type': 'sleep', 'y_data_type': 'eat'}
-    correlation_output = {}
+    correlation_output = {
+        'user_id': 1,
+        'x_data_type': 'sleep',
+        'y_data_type': 'eat',
+        'correlation': {
+            'value': 1.0,
+            'p_value': 1.0,
+        },
+    }
     r = requests.post(app_url + '/calculate', json=calculate_input)
 
     r = requests.get(app_url + '/correlation', params=correlation_input)
